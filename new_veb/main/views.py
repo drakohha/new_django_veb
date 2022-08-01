5b24d4cb88edbdbd16cc56b945fa0ec12db90a59
drakonha <drakohha@tut.by>
2022-08-01 14:13:01 +0300

    dz new html

--- new_veb/main/views.py
+++ new_veb/main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>hello</h4>")
+    return render(request,'index.html')
 
 def about(request):
-    return HttpResponse("<h4>About</h4>")
+    return render(request,'about.html')
+
