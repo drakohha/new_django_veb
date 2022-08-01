b9dce0f3a2dfffc572f9145fadaa5d0bc5fb9f12
drakonha <drakohha@tut.by>
2022-08-01 13:50:29 +0300

    dz new page

--- new_veb/main/urls.py
+++ new_veb/main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('about-us',views.about)
 ]
--- new_veb/main/views.py
+++ new_veb/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return HttpResponse("<h4>hello</h4>")
+
+def about(request):
+    return HttpResponse("<h4>About</h4>")
