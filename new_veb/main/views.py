b76337feb50ed5bdc871c27ce6ec5ea616ba5144
drakonha <drakohha@tut.by>
2022-08-01 16:28:47 +0300

    dz new bd tasks in home page

--- new_veb/main/views.py
+++ new_veb/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request,'index.html')
+    task=Task.objects.all()
+    return render(request,'index.html', {'title': 'Главная страница','tasks':'title'})
 
 def about(request):
     return render(request,'about.html')
