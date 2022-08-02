8530da30fabed0ed79208a93a47be6e53226cd00
drakonha <drakohha@tut.by>
2022-08-02 11:10:42 +0300

    dz new field

--- new_veb/main/forms.py
+++ new_veb/main/forms.py
@@ -1,7 +1,11 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 class TaskForm(ModelForm):
     class Meta:
         model=Task
         fields=["title","task"]
+        widgets = {"title":TextInput(attrs={'class':'form-control', 'placeholder':'Введите название'}),
+                   "task":TextInput(attrs={'class':'form-control', 'placeholder':'Введите название'}),
+                   }
+
