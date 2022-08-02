5f1450a0116e2fa30c6136c523dce9e97ad5be65
drakonha <drakohha@tut.by>
2022-08-02 11:40:39 +0300

    dz final

--- new_veb/main/models.py
+++ new_veb/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Task(models.Model):
     title=models.CharField("Название",max_length=50)
     task=models.TextField("Описание")
-    data=models.DateTimeField(auto_now_add="")
+
 
 
 
--- new_veb/main/views.py
+++ new_veb/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -11,11 +11,20 @@ def about(request):
     return render(request,'about.html')
 
 def create(request):
+    error=''
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'форма была не верной'
     form=TaskForm()
-
     context = {
-        'form':form
+        'form':form,
+        'error':error
     }
-    return render(request,'create.html')
+    return render(request,'create.html',context)
+
 
 
